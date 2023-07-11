ea96ee410ffc934d2d4dcf97058ba79e6c71a5d0
cletusola <olamictus@outlook.com>
2023-07-11 06:07:13 -0400

    modified geo_app/forms.py

--- geo_app/forms.py
+++ geo_app/forms.py
@@ -10,9 +10,9 @@ class AddressForm(forms.Form):
     def clean_ip_address(self):
         ip_address = self.cleaned_data["ip_address"]
 
-        if not all(i.is_digit() for i in ip_address ):
-            return forms.ValidationError(_("Ip address must be a number"))
+        # if not all(i.isdigit() for i in ip_address ):
+        #     return forms.ValidationError("Ip address must be a number")
         if ip_address == " ":
-            return forms.ValidationError(_("Ip cannot be empty"))
+            return forms.ValidationError("Ip cannot be empty")
         
         return ip_address
