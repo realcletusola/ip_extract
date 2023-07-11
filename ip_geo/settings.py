a2b65840a7a9ab0cfb935b8bab97ef29c4a64714
cletusola <olamictus@outlook.com>
2023-07-11 06:08:24 -0400

    modified ip_geo/settings.py

--- ip_geo/settings.py
+++ ip_geo/settings.py
@@ -104,9 +104,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_DIRS = [ os.path.join(BASE_DIR, "static"), os.path.join(BASE_DIR, "geo_app/static")]
-STATIC_ROOT = os.path.jon(BASE_DIR, "staticfiles")
+STATIC_URL = '/static/'
+STATICFILES_DIRS = ((os.path.join(BASE_DIR, "ip_geo/static")), (os.path.join(BASE_DIR, "geo_app/static")),)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
